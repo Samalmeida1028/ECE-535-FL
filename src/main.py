878f7a562bb357df356a3e59bbf24811a7a4c73d
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-11-06 16:29:27 -0500

    updated main according to docs

--- src/main.py
+++ src/main.py
@@ -4,31 +4,16 @@ import logging
 import os
 import warnings
 import torch
-from mpi4py import MPI
 from fl import FL
 
-
-# For MPI experiments
-COMM = MPI.COMM_WORLD
-RANK = COMM.Get_rank()
-
-
-def main():
-    is_mpi = COMM.Get_size() != 1
-    config = read_config()
-    fl = FL(config, is_mpi, RANK)
-    fl.start()
-
-
 def read_config():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--config", type=str,
-                            help="name of the config file of simulation")
-    args = arg_parser.parse_args()
     config = configparser.ConfigParser()
-    config.read(args.config)
+    config.read('/media/sf_ece535SLAM/opp/dccae/A0_B0_AB30_label_AB_test_B')
+
     return config
 
+config = read_config()
 
-if __name__ == "__main__":
-    main()
+fl = FL(config)
+
+fl.start()

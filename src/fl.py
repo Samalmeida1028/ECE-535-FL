69bbe620f3432f8c559049ecd288fcdbe4479852
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-12-03 17:27:04 -0500

    update 2

--- src/fl.py
+++ src/fl.py
@@ -27,7 +27,7 @@ class FL:
         """Starts the FL communication rounds between the server and clients."""
 
         # Loads the training and testing data of the FL simumation
-        data_name,data_train, data_test = load_data(self.config)
+        data_name, data_train, data_test = load_data(self.config)
 
         client_train = data_train
         server_test = data_test
@@ -35,26 +35,28 @@ class FL:
         y_samples = np.expand_dims(data_test["y"], axis=0) # true labels for classes
         y_samples_train = np.expand_dims(data_train["y"], axis=0) # true labels for classes
         extra_length = len(np.unique(y_samples))
+        
         y_dist= np.unique(y_samples, return_counts=True)
         y_dist_train = np.unique(y_samples_train, return_counts=True)
 
-        test_dist = {"name":data_name,"trained classes": y_dist[0].tolist(),"class counts" : y_dist[1].tolist()}
-        train_dist = {"name":data_name,"trained classes": y_dist_train[0].tolist(),"class counts" : y_dist_train[1].tolist()}
+        test_dist = {"name": data_name,"trained classes": y_dist[0].tolist(),"class counts" : y_dist[1].tolist()}
+        train_dist = {"name": data_name,"trained classes": y_dist_train[0].tolist(),"class counts" : y_dist_train[1].tolist()}
+
         try:
-            with open("distributions/test_dist" + data_name + ".txt", 'w') as f:
+            with open("distributions/test_dist_" + data_name + ".txt", 'w') as f:
                 f.write(json.dumps(test_dist))
         except Exception as e:
             print(e, "creating file")
-            with open("distributions/test_dist" + data_name + ".txt", 'x') as f:
+            with open("distributions/test_dist_" + data_name + ".txt", 'x') as f:
                 f.write(json.dumps(test_dist))
 
         try:
-            with open("distributions/train_dist" + data_name + ".txt", 'w') as f:
-                f.write(json.dumps(test_dist))
+            with open("distributions/train_dist_" + data_name + ".txt", 'w') as f:
+                f.write(json.dumps(train_dist))
         except Exception as e:
             print(e, "creating file")
-            with open("distributions/train_dist" + data_name + ".txt", 'x') as f:
-                f.write(json.dumps(test_dist))
+            with open("distributions/train_dist_" + data_name + ".txt", 'x') as f:
+                f.write(json.dumps(train_dist))
 
         # There is a small chance that the labels in the generated server_train are fewer than the labels in server_test.
         # If that happens, regenerate the server_train again until the sets of lables between them are the same.

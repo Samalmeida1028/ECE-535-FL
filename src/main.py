6563da5f225fe264bea8247d8237f71ac1b567a7
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-12-02 19:07:16 -0500

    added new results

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ from fl import FL
 
 def read_config():
     config = configparser.ConfigParser()
-    config.read('/media/sf_ece535SLAM/config/ur_fall/ablation/rgb_depth/A30_B30_AB0_label_A_test_B')
+    #config.read('/media/sf_ece535SLAM/config/ur_fall/ablation/rgb_depth/A30_B30_AB0_label_A_test_B')
     #config.read('/media/sf_ece535SLAM/config/mhealth/ablation/gyro_mage/A30_B30_AB0_label_A_test_B')
-    #config.read('/media/sf_ece535SLAM/config/opp/dccae/A0_B0_AB30_label_AB_test_B')
+    config.read('/media/sf_ece535SLAM/config/dccae/acce_gyro/A30_B30_AB0_label_A_test_B')
 
     return config
 

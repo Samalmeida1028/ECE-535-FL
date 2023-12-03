b134cee593c63ee99a62e7d73385a1def946e356
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-12-02 20:29:30 -0500

    plot generated for test set 1

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-fig,ax = plt.subplots()
+plt.figure(1)
 
 with open("results/ur_fall/ablation/rgb_depth/A30_B30_AB0_label_A_test_B/results.txt", 'r') as f:
     data = f.readlines()
@@ -14,25 +14,19 @@ yticksa = []
 for line in data:
     linein = line.strip().split(sep=",")
     timestamp.append(float(linein[0]))
-    data1.append(linein[7])
-    data2.append(linein[8])
-    data3.append(linein[9])
+    data1.append(float(linein[7]))
+    data2.append(float(linein[8]))
+    data3.append(float(linein[9]))
 
-# for i in range((3*len(data1))):
-#     yticksa.append(round(i/(3*len(data1)),2))
+plt.plot(timestamp, data1)
+plt.plot(timestamp, data2)
+plt.plot(timestamp, data3)
 
-ax.plot(timestamp, data1)
-ax.plot(timestamp, data2)
-ax.plot(timestamp, data3)
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.yticks(['0','0.5','1.0'])
-# plt.ylim(ymax=1)
-# ax.set_yticks(['0', '.5', '1.0'])
-# ax.set_yscale('linear')
-# ax.set_yticklabels(['0','.5','1.0'],fontsize=5)
-plt.autoscale('both')
-# ax.set_yt
-# ax.set_autoscaley_on(True)
-plt.title('Graph from text file')
+plt.xlabel('Training Round')
+plt.ylabel('Accuracy')
+
+plt.yticks([0, 0.5, 1.0], ['0', '0.5', '1.0'])
+plt.legend(['Class 0', 'Class 1', 'Class 2'])
+
+plt.title('UmFL_A30_B30_LA_TB')
 plt.show()
--- src/main.py
+++ src/main.py
@@ -1,16 +1,11 @@
 import configparser
-import argparse
-import logging
-import os
-import warnings
-import torch
 from fl import FL
 
 def read_config():
     config = configparser.ConfigParser()
     #config.read('/media/sf_ece535SLAM/config/ur_fall/ablation/rgb_depth/A30_B30_AB0_label_A_test_B')
     #config.read('/media/sf_ece535SLAM/config/mhealth/ablation/gyro_mage/A30_B30_AB0_label_A_test_B')
-    config.read('/media/sf_ece535SLAM/config/dccae/acce_gyro/A30_B30_AB0_label_A_test_B')
+    config.read('/media/sf_ece535SLAM/config/opp/dccae/A10_B0_AB30_label_A_test_B')
 
     return config
 

1e50e4d34c925fb873d08360fa32d1a34097d274
Sam Almeida <samuel.almeida1281@gmail.com>
2023-12-06 23:24:42 -0500

    m

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import configparser
 from fl import FL
 import os
 
-def read_config(path):
-    config = configparser.ConfigParser()
-    config.read(path)
-    return config
+# def read_config(path):
+#     config = configparser.ConfigParser()
+#     config.read(path)
+#     return config
 
 config_directory = 'config'
 
@@ -21,14 +21,14 @@ for dir, subdir,files in os.walk(config_directory):
                 # print(os.path.join(dir,direct,data,file))
 
 
-# for p in paths:
-#     print(paths)
-
-
-
-
-
 for p in paths:
-    config = read_config(p)
-    fl = FL(config)
-    fl.start()
+    print(paths)
+
+
+
+
+
+# for p in paths:
+#     config = read_config(p)
+#     fl = FL(config)
+#     fl.start()

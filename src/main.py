1fcf1c2a569effa177063058421f50282546e5be
Sam Almeida <samuel.almeida1281@gmail.com>
2023-12-08 12:58:12 -0500

    todo change logic

--- src/main.py
+++ src/main.py
@@ -2,31 +2,19 @@ import configparser
 from fl import FL
 import os
 
-# def read_config(path):
-#     config = configparser.ConfigParser()
-#     config.read(path)
-#     return config
+def read_config(path):
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
 
 config_directory = 'config'
 
 paths = []
-for dir, subdir,files in os.walk(config_directory):
+for dir, subdir, files in os.walk(config_directory):
+    print(subdir)
     for sdir in subdir:
         for file in files:
             paths.append(os.path.join(dir,sdir,file))
-
-        # for data in direct:
-        #     for file in data:
-        #         pass
-                # print(os.path.join(dir,direct,data,file))
-
-
-for p in paths:
-    print(paths)
-
-
-
-
 
 # for p in paths:
 #     config = read_config(p)

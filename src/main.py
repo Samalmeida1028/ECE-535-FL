9bf290198fc379fc89895982cd66274a1db65630
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-12-09 15:42:59 -0500

    All configs set up to run

--- src/main.py
+++ src/main.py
@@ -2,33 +2,25 @@ import configparser
 from fl import FL
 import os
 
-# def read_config(path):
-#     config = configparser.ConfigParser()
-#     config.read(path)
-#     return config
+def read_config(path):
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
 
 config_directory = 'config'
-
 paths = []
-for dir, subdir,files in os.walk(config_directory):
-    for sdir in subdir:
+
+for dir, subdir, files in os.walk(config_directory):
+    if len(subdir) == 0:
         for file in files:
-            paths.append(os.path.join(dir,sdir,file))
-
-        # for data in direct:
-        #     for file in data:
-        #         pass
-                # print(os.path.join(dir,direct,data,file))
-
-
-for p in paths:
-    print(paths)
-
-
-
-
+            paths.append(os.path.join(dir, file))
 
 # for p in paths:
-#     config = read_config(p)
-#     fl = FL(config)
-#     fl.start()
+#     print(p)
+
+# print(len(paths)) # 98 configs * 100 runs per config = 9800 runs total ~ 24 hours tops
+
+for p in paths:
+    config = read_config(p)
+    fl = FL(config)
+    fl.start()
